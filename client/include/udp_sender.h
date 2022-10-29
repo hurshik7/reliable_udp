ee18815782d7c0193bb6931c1c97575a1872b9b5
hurshik7 <hurshik7@gmail.com>
2022-10-29 03:00:46 -0700

    move creade_rudp_packet_malloc, implement init_rudp_header

--- client/include/udp_sender.h
+++ client/include/udp_sender.h
@@ -20,7 +20,7 @@ struct options;
 int init_sockaddr(struct sockaddr_in *addr);
 int init_proxy_sockaddr(struct sockaddr_in *proxy_addr, const struct options *opts);
 int do_client(const struct options *opts, struct sockaddr_in *proxy_addr, const struct sockaddr_in *addr);
-rudp_packet_t *create_rudp_packet_malloc(const rudp_header_t *header, size_t data_length, const char *data);
 
 
 #endif //UDP_CLIENT_UDP_SENDER_H
+
